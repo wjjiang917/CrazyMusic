aadce443702fbe0ce4b1260efa52a5ac6453d017
Jiangwenjin <jiang@thankyo.net>
2017-03-13 20:36:16 +0500

    my

--- app/src/main/java/me/crazyjiang/crazymusic/ui/main/activity/MainActivity.java
+++ app/src/main/java/me/crazyjiang/crazymusic/ui/main/activity/MainActivity.java
@@ -3,10 +3,16 @@ package me.crazyjiang.crazymusic.ui.main.activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -15,7 +21,9 @@ import me.crazyjiang.crazymusic.R;
 import me.crazyjiang.crazymusic.presenter.MainPresenter;
 import me.crazyjiang.crazymusic.presenter.contract.MainContract;
 import me.crazyjiang.crazymusic.ui.BaseActivity;
+import me.crazyjiang.crazymusic.ui.ViewPagerAdapter;
 import me.crazyjiang.crazymusic.ui.main.fragment.FooterFragment;
+import me.crazyjiang.crazymusic.ui.main.fragment.MyFragment;
 
 public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.View {
     @BindView(R.id.drawer_layout)
@@ -24,11 +32,17 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
     Toolbar mToolbar;
     @BindView(R.id.nav_view)
     NavigationView mNavigationView;
+    @BindView(R.id.main_viewpager)
+    ViewPager mViewPager;
+    @BindView(R.id.tab_main)
+    TabLayout mTabLayout;
 
     @Inject
     FooterFragment footerFragment;
 
     private ActionBarDrawerToggle mDrawerToggle;
+    private ViewPagerAdapter mAdapter;
+    private List<Fragment> fragments = new ArrayList<>();
 
     @Override
     protected int getLayoutResId() {
@@ -38,7 +52,6 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -54,6 +67,14 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
         mDrawerLayout.addDrawerListener(mDrawerToggle);
 
         loadRootFragment(R.id.bottom_container, footerFragment);
+
+        fragments.add(new MyFragment());
+
+        mAdapter = new ViewPagerAdapter(getSupportFragmentManager(), fragments);
+        mViewPager.setAdapter(mAdapter);
+
+        mTabLayout.addTab(mTabLayout.newTab());
+        mTabLayout.setupWithViewPager(mViewPager);
 
         mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
